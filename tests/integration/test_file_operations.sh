7c0454e08fe8a03a30540e10c431073a5dcaa374
wangzhao <wangzhao@megvii.com>
2023-07-04 14:43:58 +0800

    test: integration test

---
+++ tests/integration/test_file_operations.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e # exit if any command fails
+set -x # print each command before executing
+
+# Define cleanup procedure
+cleanup() {
+    echo "Cleaning up..."
+    popd || true
+    umount -l /tmp/vfs-mount || true
+    kill $vfs_pid || true
+    sleep 1
+    rm -rf /tmp/vfs-root /tmp/vfs-mount
+}
+
+# Register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+# Mount the file system
+rm -rf /tmp/vfs-root /tmp/vfs-mount
+mkdir /tmp/vfs-root
+mkdir /tmp/vfs-mount
+vfs-fuse /tmp/vfs-root /tmp/vfs-mount & # run in the background
+vfs_pid=$!
+
+# Give it a moment to mount
+sleep 5
+
+# Go to the mount point
+pushd /tmp/vfs-mount
+
+# Create a new file
+echo "Hello, World!" > test.txt
+[ "$(cat test.txt)" = "Hello, World!" ] || { echo "File contents mismatch"; exit 1; }
+
+# Modify the file
+echo "Goodbye, World!" > test.txt
+[ "$(cat test.txt)" = "Goodbye, World!" ] || { echo "File contents mismatch"; exit 1; }
+
+# Remove the file
+rm test.txt
+[ -f test.txt ] && { echo "File still exists after removal"; exit 1; }
+
+# View the commit history (should be 4 commits)
+echo "Commit history:"
+git --no-pager log --oneline
+[ "$(git log --pretty=oneline | wc -l)" -eq 4 ] || { echo "Commit history mismatch"; exit 1; }
