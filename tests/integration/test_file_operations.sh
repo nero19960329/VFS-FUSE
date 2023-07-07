18665fb636d87cb993f59bc1b1325a35a5ff4959
wangzhao <wangzhao@megvii.com>
2023-07-07 15:47:05 +0800

    feat: support rename

--- tests/integration/test_file_operations.sh
+++ tests/integration/test_file_operations.sh
@@ -39,17 +39,27 @@ echo "Hello, World!" > test.txt
 echo "Goodbye, World!" > test.txt
 [ "$(cat test.txt)" = "Goodbye, World!" ] || { echo "File contents mismatch"; exit 1; }
 
+# Rename the file
+mv test.txt test2.txt
+[ -f test.txt ] && { echo "File still exists after rename"; exit 1; }
+[ "$(cat test2.txt)" = "Goodbye, World!" ] || { echo "File contents mismatch"; exit 1; }
+
 # Remove the file
-rm test.txt
-[ -f test.txt ] && { echo "File still exists after removal"; exit 1; }
+rm test2.txt
+[ -f test2.txt ] && { echo "File still exists after removal"; exit 1; }
 
 # Create a new directory
 mkdir test
 [ -d test ] || { echo "Directory not created"; exit 1; }
 
+# Rename the directory
+mv test test2
+[ -d test ] && { echo "Directory still exists after rename"; exit 1; }
+[ -d test2 ] || { echo "Directory not renamed"; exit 1; }
+
 # Remove the directory
-rm -rf test
-[ -d test ] && { echo "Directory still exists after removal"; exit 1; }
+rm -rf test2
+[ -d test2 ] && { echo "Directory still exists after removal"; exit 1; }
 
 # Check `/tmp/vfs-mount` only has the `.git` directory
 [ "$(ls -A /tmp/vfs-mount)" = ".git" ] || { echo "Unexpected files in /tmp/vfs-mount"; exit 1; }
